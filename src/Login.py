98cdaf56cfc4566a2c0f0003778568fd977f67dc
Tiven <vairtor@gmail.com>
2023-02-02 14:07:47 -0300

    Add users and store them in memory

--- src/Login.py
+++ src/Login.py
@@ -1,15 +1,26 @@
+class User:
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+
+
+users = []
+
+
 def menu():
-    menuOp = int(input("1-Login\n2-Register\n3-Exit\nOption:"))
+    menuOp = int(input("1-Login\n2-Register\n3-Show Users\n4-Exit\nOption:"))
     match menuOp:
         case 1:
             login()
         case 2:
             register()
         case 3:
+            showUsers()
+        case 4:
             return False
         case _:
             print("You inserted wrong number")
-            return True
+            return False
 
 
 def login():
@@ -18,12 +29,27 @@ def login():
     password = input("Insert Password: ")
 
 
+def addUser(login, password):
+    user = User(login, password)
+    users.append(user)
+    menu()
+
+
+def showUsers():
+    for userf in users:
+        print(userf.name)
+    menu()
+
+
 def register():
     print("-----REGISTER-----")
     username = input("Insert Username: ")
     password = input("Insert Password: ")
+    addUser(username, password)
+    menu()
 
 
 condition = True
+
 while condition:
     condition = menu()
