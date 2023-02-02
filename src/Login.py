bbbac1cabd1ca4a8efab4c63ea8aa63e2fc01d80
Tiven <vairtor@gmail.com>
2023-02-02 14:21:54 -0300

    Fix infinite loop

--- src/Login.py
+++ src/Login.py
@@ -8,19 +8,21 @@ users = []
 
 
 def menu():
-    menuOp = int(input("1-Login\n2-Register\n3-Show Users\n4-Exit\nOption:"))
-    match menuOp:
-        case 1:
-            login()
-        case 2:
-            register()
-        case 3:
-            showUsers()
-        case 4:
-            return False
-        case _:
-            print("You inserted wrong number")
-            return False
+    while True:
+        menuOp = int(
+            input("1-Login\n2-Register\n3-Show Users\n4-Exit\nOption:"))
+        match menuOp:
+            case 1:
+                login()
+            case 2:
+                register()
+            case 3:
+                showUsers()
+            case 4:
+                return False
+            case _:
+                print("You inserted wrong number")
+                return False
 
 
 def login():
@@ -32,13 +34,11 @@ def login():
 def addUser(login, password):
     user = User(login, password)
     users.append(user)
-    menu()
 
 
 def showUsers():
     for userf in users:
         print(userf.name)
-    menu()
 
 
 def register():
@@ -46,10 +46,6 @@ def register():
     username = input("Insert Username: ")
     password = input("Insert Password: ")
     addUser(username, password)
-    menu()
 
 
-condition = True
-
-while condition:
-    condition = menu()
+menu()
